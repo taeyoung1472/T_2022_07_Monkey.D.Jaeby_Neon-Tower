ccabe2a853fbccabe575f81bfc775b7005d2a089
taeyoung1472 <77605585+taeyoung1472@users.noreply.github.com>
2022-07-11 14:28:01 +0900

    총알발사

--- Assets/01.Script/Taeyoung/Player/PlayerController.cs
+++ Assets/01.Script/Taeyoung/Player/PlayerController.cs
@@ -1,23 +1,53 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float speed;
+    [SerializeField] private float speed = 5;
+    [SerializeField] private float dashFixValue = 3;
+    [SerializeField] private float dashTime = 0.1f;
+    [SerializeField] private Transform firePos;
+    [SerializeField] private GameObject bullet;
     [SerializeField] private LayerMask layerMask;
     CharacterController controller;
     Vector3 moveDir;
     Camera cam;
+    bool isCanControll = true;
     void Start()
     {
         controller = GetComponent<CharacterController>();
         cam = Camera.main;
+
+        StartCoroutine(DashSystem());
     }
     void Update()
     {
         Move();
+        Gravity();
         Rotate();
+        Shoot();
+    }
+
+    private void Shoot()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Instantiate(bullet, firePos.position, Quaternion.Euler(0,transform.eulerAngles.y,0));
+        }
+    }
+
+    IEnumerator DashSystem()
+    {
+        while (true)
+        {
+            yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.LeftShift));
+            isCanControll = false;
+            moveDir = moveDir.normalized * dashFixValue;
+            yield return new WaitForSeconds(dashTime);
+            isCanControll = true;
+        }
     }
 
     private void Rotate()
@@ -32,8 +62,6 @@ public class PlayerController : MonoBehaviour
 
             float angle = Mathf.Atan2(hitPos.x - transform.position.x, hitPos.z - transform.position.z) * Mathf.Rad2Deg;
 
-            print(angle);
-
             transform.eulerAngles = new Vector3(0, angle, 0);
         }
     }
@@ -43,9 +71,23 @@ public class PlayerController : MonoBehaviour
         float h = Input.GetAxisRaw("Horizontal");
         float v = Input.GetAxisRaw("Vertical");
 
-        moveDir = new Vector3(h, 0, v);
-        moveDir = transform.TransformDirection(moveDir);
+        if (isCanControll)
+        {
+            moveDir = new Vector3(h, moveDir.y, v);
+        }
 
         controller.Move(moveDir * speed * Time.deltaTime);
+    }
+
+    private void Gravity()
+    {
+        if (!controller.isGrounded)
+        {
+            moveDir.y += Time.deltaTime * Physics.gravity.y;
+        }
+        else
+        {
+            moveDir.y = 0;
+        }
     }
 }
