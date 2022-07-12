ef3ae5f65235fb4d31ff1ed40680076b79af3e75
hanmin0728 <gksalsdud1234@gmail.com>
2022-07-12 18:49:55 +0900

    아이템

--- Assets/01.Script/Minyoung/Core/EnergyBall.cs
+++ Assets/01.Script/Minyoung/Core/EnergyBall.cs
@@ -4,18 +4,35 @@ using UnityEngine;
 
 public class EnergyBall : MonoBehaviour
 {
-   
+    [SerializeField]
+    private Transform targetTrm;
+    [SerializeField] private Transform[] fireBalls;
+    [SerializeField] private float sinSpeed = 1;
+    [SerializeField] private float sinValue = 1;
+    private float time;
     public void OnEnable()
     {
         StartCoroutine(ItemRotate());
     }
     private void Update()
     {
+        transform.position = targetTrm.position;
         Rotate();
     }
     void Rotate()
     {
-        transform.Rotate(new Vector3(0, 3f, 0));
+        transform.Rotate(new Vector3(0, 120f, 0) * Time.deltaTime);
+
+        time += Time.deltaTime * sinSpeed;
+
+        float timeFix = 0;
+
+        foreach (Transform fireBall in fireBalls)
+        {
+            float sin = Mathf.Sin(time + timeFix);
+            fireBall.localPosition = new Vector3(0, 0, 2) + (Vector3.forward * sin) * sinValue;
+            timeFix += 0.1f;
+        }
     }
     IEnumerator ItemRotate()
     {
