bd6ad47333f364240205a6e5969c89fae9911140
jinwoo <=>
2022-07-14 14:58:32 +0900

    ㅇ

--- Assets/01.Script/Jinwoo/etc/Exp.cs
+++ Assets/01.Script/Jinwoo/etc/Exp.cs
@@ -6,7 +6,12 @@ public class Exp : MonoBehaviour
 {
     public ParticleSystem _exp;
 
+    public float duration = 0.5f;
 
+    private void Awake()
+    {
+        _exp = GetComponent<ParticleSystem>();
+    }
 
     void Start()
     {
