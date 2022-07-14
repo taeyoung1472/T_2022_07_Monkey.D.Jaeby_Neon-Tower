f0e029a157770ff063f227535b91e3664192d60d
jinwoo <=>
2022-07-14 14:46:50 +0900

    ㄴㅇㅁ러ㅏㅣㅁㄴㄹ

---
+++ Assets/01.Script/Jinwoo/etc/Exp.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exp : MonoBehaviour
+{
+    public ParticleSystem _exp;
+
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
